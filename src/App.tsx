44e756f2358d9671f7d5ec70ef26edba38e2a376
ammarrqureshi <mamqureshi786@gmail.com>
2024-01-15 20:44:11 +0500

    feat: navbar basic setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,36 @@
-import { BaggageClaim, Columns2 } from "lucide-react";
+import { BaggageClaim, Columns2, ShoppingCart } from "lucide-react";
 import { Button } from "./components/ui/button";
+import { Input } from "./components/ui/input";
 
 function App() {
   return (
     <>
       <main className="">
         <div className="nav p-2 sticky top-0 backdrop-blur-md shadow-sm ">
-          Hello weorl:d
-          <Button variant="secondary" size="icon">
-            <Columns2 />
-          </Button>
-          <Button>
-            <BaggageClaim className="mr-3" />
-            Checkout{" "}
-          </Button>
+          <div className="flex justify-between items-center gap-8">
+            <div
+              id="nav-left"
+              className="flex justify-between items-center gap-3"
+            >
+              <Button variant="ghost" size="icon">
+                <Columns2 />
+              </Button>
+              <h1 className="text-lg font-bold text-primary"> EcoShopper</h1>
+            </div>{" "}
+            <Input placeholder="Search" value="" />
+            <div
+              id="nav-right"
+              className="flex justify-between items-center gap-3"
+            >
+              <Button variant="ghost" size="icon">
+                <ShoppingCart />
+              </Button>
+              <Button>
+                <BaggageClaim className="mr-3" />
+                Checkout{" "}
+              </Button>
+            </div>
+          </div>
         </div>
         <div className="max-w-20">
           <h1>Heading</h1>
